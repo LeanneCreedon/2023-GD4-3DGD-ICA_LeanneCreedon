0f0e62056e4afd5dd538d708f745a543b1d39638
LeanneCreedon <D00236480@student.dkit.ie>
2023-12-22 23:14:43 +0000

    :sparkles: Added Player Controller
    Point and Click Controller Implemented. Features include: Idle and Walk Animations, Camera Moving with Player, and Target Particle Effect.

---
+++ Assets/My Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,112 @@
+using UnityEngine;
+using UnityEngine.AI;
+public class PlayerController : MonoBehaviour
+{
+    const string IDLE = "Idle_A";
+    const string WALK = "Walk";
+
+    CustomActions input;
+
+    NavMeshAgent agent;
+    Animator animator;
+
+    [Header("Movement")]
+    [SerializeField] ParticleSystem clickEffect;
+    [SerializeField] LayerMask clickableLayers;
+
+    float lookRotationSpeed = 8f;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+
+        input = new CustomActions();
+        AssignInputs();
+    }
+
+    void AssignInputs()
+    {
+        input.Main.Move.performed += ctx => ClickToMove();
+    }
+
+    void ClickToMove()
+    {
+        Debug.Log("CLICKED");
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100, clickableLayers))
+        {
+            agent.destination = hit.point;
+            if (clickEffect != null)
+            { Instantiate(clickEffect, hit.point + new Vector3(0, 0.1f, 0), clickEffect.transform.rotation); }
+        }
+    }
+
+    void OnEnable()
+    { input.Enable(); }
+
+    void OnDisable()
+    { input.Disable(); }
+
+    void Update()
+    {
+        FaceTarget();
+        SetAnimations();
+    }
+
+    void FaceTarget()
+    {
+        if (agent.velocity != Vector3.zero)
+        {
+            Vector3 direction = (agent.destination - transform.position).normalized;
+            Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * lookRotationSpeed);
+        }
+    }
+
+    void SetAnimations()
+    {
+        if (agent.velocity == Vector3.zero)
+        { animator.Play(IDLE); }
+        else
+        { animator.Play(WALK); }
+    }
+}
+
+/*
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent player;
+    public Animator playerAnimator;
+    public GameObject targetDest;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitPoint;
+
+            if (Physics.Raycast(ray, out hitPoint))
+            {
+                targetDest.transform.position = hitPoint.point;
+                player.SetDestination(hitPoint.point);
+            }
+        }
+
+        if (player.velocity != Vector3.zero)
+        {
+            playerAnimator.SetBool("isWalking", true);
+        }
+        else if (player.velocity == Vector3.zero)
+        {
+            playerAnimator.SetBool("isWalking", false);
+        }
+    }
+}
+*/
